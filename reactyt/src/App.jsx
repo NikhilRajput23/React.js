09c431169d1e9631f835a0617c963e3f7846e895
Nikhil Rajput <nikhirajput3006@gmail.com>
2026-01-01 21:38:58 +0530

    addall

--- reactyt/src/App.jsx
+++ reactyt/src/App.jsx
@@ -1,4 +1,12 @@
+import React ,{useState}from "react";
+
 function App(){
-  return <h1>hello</h1>
+  const [a,setA]=useState(0);
+  return(
+  <div>
+     <h1 className="text-5xl bg-pink-700">value a is {a}</h1>
+    <button onClick={()=>setA(a+10)}>increment</button>
+     <button onClick={()=>setA(a-10)}>decrement</button>
+    </div> )
 }
 export default App
