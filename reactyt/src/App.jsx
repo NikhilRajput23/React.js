a9b6716f3fac251be96bbcc4e076a2e85eeac7e2
Nikhil Rajput <nikhirajput3006@gmail.com>
2026-01-04 19:07:35 +0530

    change

--- reactyt/src/App.jsx
+++ reactyt/src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React ,{useState} from "react";
 const App=()=>{
-  const submitH=()=>{
-    console.log("Hello World");
+  const[user,setUser]=useState("")
+  const submitH=(e)=>{
+    e.preventDefault();
+    console.log(user);
+    setUser('');
   }
-  return(<div><form className='flex flex-col' onSubmit={submitH}>
-    <input className="px-3 py-4 text-xl m-10"type="text" placeholder='Enter a Input'/>
-    <button type="submit" className='px-3 py-4 m-10 border-white rounded text-xl font-semibold'>Submit</button>
-    </form></div>)
+  return(<div className="min-h-screen bg-color-black flex items-center justify-center">
+    <form onSubmit={submitH}className="flex flex-col gap-4"
+>
+<input 
+value={user}
+onChange={(e)=>setUser(e.target.value)}
+className="px-3 py-3 text-lg rounded-lg w-[350px]" type="text"
+placeholder="Enter a name"
+></input>
+<button type="submit" className="px-3 py-3 text-lg font-semibolde border border-white text-white rounded-lg hover:bg-white hover:text-black">submit</button>
+    </form>
+  </div>)
 }
 export default App;
