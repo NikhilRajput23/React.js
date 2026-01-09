ba9ac46cf38e27fe33195ea45746cd6a07ba41cb
Nikhil Rajput <nikhirajput3006@gmail.com>
2026-01-09 21:46:33 +0530

    add

--- reactyt/src/App.jsx
+++ reactyt/src/App.jsx
@@ -1,10 +1,28 @@
 import React ,{useState} from "react";
-import Card from "./components/Card"
+import Card from "./components/Card";
+import axios from  'axios';
 const App=()=>{
-  const user=11;
-  return(<div>
-    <Card a='10'/>
-     <Card a='11'/>
-     </div>)
+  const[data,setData]=useState([])
+  const getData=async()=>{
+    const response=await axios.get('https://picsum.photos/v2/list');
+    setData(response.data);
+  }
+  return(
+    <div className="p-10">
+    <button onClick={getData}className='bg-teal-600 text-white font-semibold text-xl rounded px-6 py-6'>get data</button>
+    <div className="p-5 m-5 bg-yellow-950">
+{
+  data.map(function (elem,idx){
+
+    return(<div key={idx} className="bg-white-50 text-black flex items-center gap-6 w-full px-6 py-7 rounded  mb-3">
+      <img className="h-80 "src={elem.download_url} alt=""/>
+      <h1 className="text-xl font-semibold">{elem.author}</h1>
+    </div>)
+  })
+}
+
+    </div>
+    </div>
+  )
 }
 export default App;
