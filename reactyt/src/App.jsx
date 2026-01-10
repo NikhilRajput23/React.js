5a1a2d6ec6b8a40fad7fc270acae617a0d1838af
Nikhil Rajput <nikhirajput3006@gmail.com>
2026-01-10 20:18:56 +0530

    change_something

--- reactyt/src/App.jsx
+++ reactyt/src/App.jsx
@@ -1,28 +1,32 @@
-import React ,{useState} from "react";
+import React ,{useEffect, useState} from "react";
 import Card from "./components/Card";
 import axios from  'axios';
 const App=()=>{
-  const[data,setData]=useState([])
-  const getData=async()=>{
+  const[data ,setData]=useState([])
+   const getData=async()=>{
     const response=await axios.get('https://picsum.photos/v2/list');
-    setData(response.data);
-  }
-  return(
-    <div className="p-10">
-    <button onClick={getData}className='bg-teal-600 text-white font-semibold text-xl rounded px-6 py-6'>get data</button>
-    <div className="p-5 m-5 bg-yellow-950">
-{
-  data.map(function (elem,idx){
+    setData(response.data)
+   };
+   useEffect(()=>{
+    getData();
 
-    return(<div key={idx} className="bg-white-50 text-black flex items-center gap-6 w-full px-6 py-7 rounded  mb-3">
-      <img className="h-80 "src={elem.download_url} alt=""/>
-      <h1 className="text-xl font-semibold">{elem.author}</h1>
-    </div>)
-  })
-}
+   },[]);
+   return(
+   <div className="p-10">
+    <div className="p-5 mt-5 bg-yellow-500 rounded">
+      {
+        data.map(function(elem,id){
+          return(<div key={id} className="bg-white-50 text-black flex items-center gap-6 w-full px-6 py-7 rounded  mb-3">
+              <img className="h-80"src={elem.download_url} alt="" />
+              <h1>{elem.author}</h1>
+          </div>)
+
+        })
+      }
 
     </div>
-    </div>
-  )
-}
+     </div>
+ 
+   );
+};
 export default App;
